f7dc5332c0791d247c88eb976d68fe5de735be8f
Jason Creighton & Ike Mattice <student@epicodus.com>
2015-08-11 11:48:39 -0700

    return second test success matches found.

--- src/AnagramChecker.php
+++ src/AnagramChecker.php
@@ -3,7 +3,14 @@
     {
         function makeAnagram($input_word, $input_check_list)
         {
-            return $input_check_list;
+            $matches = "";
+            if ($input_word == $input_check_list)
+            {
+                $matches = $input_check_list;
+            } else {
+                $matches = "No matches found";
+            }
+            return $matches;
         }
     }
 ?>
